ef4212af658a1595f90b7c952b68da3bc2bacc5d
Justin Garcia <purefunctor@gmail.com>
2024-05-12 02:59:01 +0800

    fix: add missing base in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default defineConfig({
+  base: "/reason-react-country-select/",
   plugins: [nodeResolve()],
 });
